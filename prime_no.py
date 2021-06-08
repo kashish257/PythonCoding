ec54180309ff4755fac789ffb991fcb09c1f59ef
kashish sahu <kashish.sahu475@gmail.com>
2021-06-08 18:12:55 +0530

    change in logic

--- prime_no.py
+++ prime_no.py
@@ -1,9 +1,12 @@
-user_in=input("enter the no")
+user_in=int(input("enter the no"))
 def prime(user_in):
-    for i in range(1,11):
-        if user_in % i==1:
-            print("no is prime")
+    for i in range(2,user_in):
+        if (user_in%i)==0:
+            print('no is not prime')
+            break
 
-        else:
-            print("no. is not prime")
-prime(6)
+    else:
+        print("prime",user_in)
+
+
+
