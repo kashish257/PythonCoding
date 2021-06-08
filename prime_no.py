d85e29cf9f61989dd9ce316808109335552759df
kashish sahu <kashish.sahu475@gmail.com>
2021-06-08 16:30:12 +0530

    program is created

--- prime_no.py
+++ prime_no.py
@@ -1,1 +1,9 @@
+user_in=input("enter the no")
+def prime(user_in):
+    for i in range(1,11):
+        if user_in % i==1:
+            print("no is prime")
 
+        else:
+            print("no. is not prime")
+prime(6)
