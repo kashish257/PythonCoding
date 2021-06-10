859ac166704c73edf528670c0f3309a8fbc5e7f3
kashish sahu <kashish.sahu475@gmail.com>
2021-06-10 19:00:02 +0530

    add new shape star program file

--- star.py
+++ star.py
@@ -6,3 +6,16 @@ def star(num):
         for j in range(i+1):
             print("*",end==" ")
         print("\n")
+------------------------------------------
+
+def star():
+    for i in range(5,0,-1):
+        for z in range(1,i+1) :
+            print("*",end=" ")
+        for j in range(1,6-i):
+            print(" ",end=" ")
+
+        print("\n")
+
+
+
